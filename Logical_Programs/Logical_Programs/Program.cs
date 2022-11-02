b5ceddeb3b9fd4d6e75eb5be92588da8c53303fa
rajshri-priya <rajshri.priya1708@.gmail.com>
2022-11-02 20:11:23 +0530

    Fibonacci(0 1 1 2 3 5 ....)

--- Logical_Programs/Logical_Programs/Program.cs
+++ Logical_Programs/Logical_Programs/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public class Logical_Programs
+{ 
+    public static void Main(string[] args)
+    {
+        int n1 = 0, n2 = 1, n3, i, number;//variable declaration
+
+        Console.Write("Enter the number of elements: ");
+        number = int.Parse(Console.ReadLine());// parse method returns converted value .
+                                               // or we use res=convert.ToInt32(val)
+        Console.Write(n1 + " " + n2 + " "); //printing 0 and 1    
+        for (i = 2; i < number; ++i) //loop starts from 2 because 0 and 1 are already printed    
+        {
+            n3 = n1 + n2;
+            Console.Write(n3 + " ");
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+}
